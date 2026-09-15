cc2025caf6b422d8195cb950ea9d03239a078cec
Imaad Charles <Imaad Charles@DESKTOP-L7I72VG>
2026-09-15 18:00:22 +0200

    formatted file structure and added basic variables

---
+++ src/test/java/za/ac/iie/prog5121/MainTest.java
@@ -0,0 +1,9 @@
+public class Login {
+
+    // User registration details
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+}
